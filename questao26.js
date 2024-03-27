4e25bfdd63c755bba82bdcabcbe68933b070e4bc
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 11:17:36 -0400

    questao 26

---
+++ questao26.js
@@ -0,0 +1,4 @@
+/* (26. Explique como os parênteses afetam o resultado de uma expressão com 
+operadores lógicos em JavaScript.
+
+os parenteses afetam porque eles tem que estar antes das outras sentenças */
