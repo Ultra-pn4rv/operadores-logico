07758fa352e6e75f00e598c9c54d25c6168168b3
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 11:17:23 -0400

    questao 24

---
+++ questao24.js
@@ -0,0 +1,6 @@
+/* 24. Como você pode verificar se um número é múltiplo de 3 ou 5 em 
+JavaScript usando operadores lógicos? */
+
+    function Multipl0(numero) {
+    return numero % 3 === 0 || numero % 5 === 0;
+    }
